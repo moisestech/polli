66984f45cc6e4262214a3e3988a1c80548443728
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-03 09:44:09 -0400

    imp combineReducers result, createStore(reducer), pass store prop to <Provider>, imp LoadingBar in ColorfulBorder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
 import * as ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components";
 
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-
-// const store = createStore();
+import reducer from "./reducers";
+import LoadingBar from "react-redux-loading-bar";
 
 function ColorfulBorder() {
   return (
-    <React.Fragment>
+    <>
       <ul className="border-container">
         <li className="border-item" style={{ background: "var(--red)" }} />
         <li className="border-item" style={{ background: "var(--blue)" }} />
@@ -19,14 +17,17 @@ function ColorfulBorder() {
         <li className="border-item" style={{ background: "var(--yellow)" }} />
         <li className="border-item" style={{ background: "var(--aqua)" }} />
       </ul>
-    </React.Fragment>
+      <LoadingBar />
+    </>
   );
 }
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     <ColorfulBorder />
     <App />
-  </>,
+  </Provider>,
   document.getElementById("root")
 );
