1fee50301a1fbb6fe272b88820db858846e5f881
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-07 06:41:40 -0400

    Logo icon and header call to action func component

---
+++ src/components/Logo/index.js
@@ -0,0 +1,10 @@
+export default function Logo() {
+  return (
+    <div className="logo-header">
+      <span className="logo">
+        <img src="https://raw.githubusercontent.com/moisestech/polli/main/public/assets/polli_logo.png" />
+      </span>
+      <p>Vote, Get Insights</p>
+    </div>
+  );
+}
