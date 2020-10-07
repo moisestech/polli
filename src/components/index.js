7185d06ef42767fde4d611519d78329686b9d0bd
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-07 05:37:34 -0400

    imp handleInitialData useDispatch, useEffect dispatch(handleInitialData(), store = useSelector((store)) return loading === true

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,25 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { handleInitialData } from "../actions/shared";
 
 export default function App() {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.authedUser === null);
+
+  useEffect(() => {
+    dispatch(handleInitialData());
+  }, [dispatch]);
+
   const store = useSelector((store) => store);
 
   console.log("store", store);
 
   return (
-    <div className="app">
-      <h1>Welcome to Polli App</h1>
-      <img src="" />
+    <div className="app container">
+      {loading == true ? null : <h1>Welcome to Polli App</h1>}
+      <span className="logo">
+        <img src="https://raw.githubusercontent.com/moisestech/polli/main/public/assets/polli_logo.png" />
+      </span>
     </div>
   );
 }
