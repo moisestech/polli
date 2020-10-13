f58e70d338ce51d9727fb448fb5f0b3d50ff3770
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-13 08:17:42 -0400

    users useSelector((state) => state.users)

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 export default function Dashboard() {
   const [list, setList] = useState("unanswered");
   const authedUser = useSelector((state) => state.authedUser);
-  const users = useSelector((state) => state.state.users);
-  const polls = useSelector((state) => state.state.polls);
+  const users = useSelector((state) => state.users);
+  const polls = useSelector((state) => state.polls);
 
   const answers = users[authedUser].answers;
   const answered = answers
@@ -15,7 +15,7 @@ export default function Dashboard() {
 
   const unanswered = Object.keys(polls)
     .filter((id) => !answers.includes(id))
-    .map((id) => poll[id])
+    .map((id) => polls[id])
     .sort((a, b) => b.timestamp - a.timestamp);
 
   const questions = { answered, unanswered };
