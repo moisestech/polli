6cf6953d475098928a1c305b62b45d745e5c3048
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-13 07:14:26 -0400

    NavLink Home Leaderboard AddPoll, imp Logo comp

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,13 +5,13 @@ export default function Nav() {
   return (
     <nav className="nav">
       <Logo />
-      <NavLink to="/" exact actvieClassName="active">
+      <NavLink to="/" exact activeClassName="active">
         Home
       </NavLink>
-      <NavLink to="/leaderboard" exact actvieClassName="active">
+      <NavLink to="/leaderboard" exact activeClassName="active">
         Leaderboard
       </NavLink>
-      <NavLink to="/add" exact actvieClassName="active">
+      <NavLink to="/add" exact activeClassName="active">
         Add Poll
       </NavLink>
     </nav>
